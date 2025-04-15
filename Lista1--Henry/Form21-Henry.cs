188cc6e417c55d39e3663e195ea44bf2b8dc97a1
Henry <henry.leite01@etec.sp.gov.br>
2025-04-15 18:11:51 -0300

    Falta 18,19 e 20

---
+++ Lista1--Henry/Form21-Henry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista1__Henry
+{
+    public partial class Form21_Henry: Form
+    {
+        public Form21_Henry()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name, sobre, completo;
+            name = Convert.ToString(textBox1.Text);
+            sobre = Convert.ToString(textBox2.Text);
+            completo = string.Concat("Olá, ",name, sobre, "!");
+            label4.Text = completo.ToString(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            label4.Text = "_____________";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
